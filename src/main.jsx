a4d97932cea210877f894dc929ef4873c5b8cca5
Juan Pablo Tocarruncho <jtocarruncho07@gmail.com>
2025-07-02 22:54:26 -0500

    ,,,

--- src/main.jsx
+++ src/main.jsx
@@ -5,12 +5,6 @@ import App from './App.jsx'
 import './index.css'
 import { registerServiceWorker } from './registerSW'
 
-registerServiceWorker(() => {
-  if (window.confirm('¡Hay una nueva versión disponible! ¿Actualizar ahora?')) {
-    window.location.reload();
-  }
-});
-
 // Registrar service worker solo para admin
 if ('serviceWorker' in navigator) {
   window.addEventListener('load', () => {
@@ -23,6 +17,12 @@ if ('serviceWorker' in navigator) {
       });
   });
 }
+
+registerServiceWorker(() => {
+  if (window.confirm('¡Hay una nueva versión disponible! ¿Actualizar ahora?')) {
+    window.location.reload();
+  }
+});
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
